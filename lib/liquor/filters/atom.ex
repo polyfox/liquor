c8738f1475b53f773b5398d01ce36e513fa9964d
Corey Powell <mistdragon100@gmail.com>
2018-11-08 13:17:04 -0500

    Forgot to import Ecto.Query

--- lib/liquor/filters/atom.ex
+++ lib/liquor/filters/atom.ex
@@ -2,6 +2,8 @@ defmodule Liquor.Filters.Atom do
   @moduledoc """
   Handles atom filters, this also affects booleans
   """
+  import Ecto.Query
+
   def apply_filter(query, op, key, value) do
     case op do
       :match -> where(query, [r], field(r, ^key) == ^value)
