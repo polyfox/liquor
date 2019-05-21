20be9ce4dc887c5298cee380e32a37f6b3eeb073
Corey Powell <mistdragon100@gmail.com>
2019-05-21 13:53:30 -0500

    Filename typo

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Liquor.MixProject do
   def project do
     [
       app: :liquor,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
